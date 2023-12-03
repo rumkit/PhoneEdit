298de35334e2851a1958ee6c6bfb8958e3fa9cbc
Mikhail Shashin <shashinma@icloud.com>
2023-12-03 21:10:42 +0300

    Update SampleData.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- Data/SampleData.cs
+++ Data/SampleData.cs
@@ -3,24 +3,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PhoneEdit.Data;
 
-public static class SampleData
+public class SampleData
 {
+    private static IConfiguration _configuration;
+
+    public SampleData(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     // Creates default app's admin
     public static async Task CreateDefaultUser(IServiceProvider serviceProvider)
     {
         var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        
+        var defaultUsername = configuration.GetValue<string>("DefaultUsername");
+        var defaultPassword = configuration.GetValue<string>("DefaultPassword");
 
         var userCheck = await userManager.Users.AnyAsync();
         if (!userCheck)
         {
             var adminUser = new IdentityUser()
             {
-                UserName = "mail@example.com",
-                Email = "mail@example.com",
+                UserName = Environment.GetEnvironmentVariable("USERNAME") ?? defaultUsername,
+                Email = Environment.GetEnvironmentVariable("USERNAME") ?? defaultUsername,
                 EmailConfirmed = true
             };
 
-            await userManager.CreateAsync(adminUser, "123456");
+            await userManager.CreateAsync(adminUser, (Environment.GetEnvironmentVariable("PASSWORD") ?? defaultPassword) ?? throw new InvalidOperationException());
         }
     }
 }
